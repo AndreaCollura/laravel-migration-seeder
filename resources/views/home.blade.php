169e83787148e45bcd551ecc9c49a19c9d3608e4
AndreaCollura <andrea.collur@gmail.com>
2023-05-24 18:47:24 +0200

    added table to home (better style to add)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,36 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h1>Placeholder</h1>
-
-        <ul>
-            @foreach ($trains as $train)
-                <li>{{$train->agency}}</li>
-                <li>{{$train->departure_station}}</li>
-                <li>{{$train->arrival_station}}</li>
-                <li>{{$train->arrival_eta}}</li>
-                <li>{{$train->train_code}}</li>
-                <li>{{$train->n_wagons}}</li>
-                <li>{{$train->on_time}}</li>
-                <li>{{$train->cnx}}</li>
-            @endforeach
-        </ul>
+        <h1 class="mt-5 text-capitalize">Trains journey</h1>
+        <div class="container my-5">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Agency</th>
+                        <th scope="col">Departure Station</th>
+                        <th scope="col">Arrival Station</th>
+                        <th scope="col">Arrival time</th>
+                        <th scope="col">train code</th>
+                        <th scope="col">wagon number</th>
+                        <th scope="col">On time</th>
+                        <th scope="col">Cancelled</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($trains as $train)
+                        <tr>
+                            <td>{{ $train->agency }}</td>
+                            <td>{{ $train->departure_station }}</td>
+                            <td>{{ $train->arrival_station }}</td>
+                            <td>{{ $train->arrival_eta }}</td>
+                            <td>n° {{ $train->train_code }}</td>
+                            <td>{{ $train->n_wagons }}</td>
+                            <td>{{ $train->on_time }}</td>
+                            <td>{{ $train->cnx }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header class="bg-white">
+    <div class="container header-box px-0">
+        <div class="d-flex justify-content-between align-items-center m-0">
+            <div class="mt-3">
+                <img src="{{ Vite::asset('resources/img/Trains_magazine_logo.svg.png') }}" alt="">
+            </div>
+        </div>
+    </div>
+</header>
