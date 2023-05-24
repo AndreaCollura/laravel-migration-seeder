846d18904faeb2506f37b53cdbe1cefc1be0cc90
AndreaCollura <andrea.collur@gmail.com>
2023-05-25 00:24:44 +0200

    more minor fix

--- resources/views/today/today.blade.php
+++ resources/views/today/today.blade.php
@@ -9,12 +9,12 @@
                         <th scope="col">Agency</th>
                         <th scope="col">Departure Station</th>
                         <th scope="col">Arrival Station</th>
-                        <th scope="col">Arrival time</th>
-                        <th scope="col">Departure station</th>
-                        <th scope="col">train code</th>
-                        <th scope="col">wagon number</th>
-                        <th scope="col">On time</th>
-                        <th scope="col">Cancelled</th>
+                        <th class="text-center" scope="col">Arrival time</th>
+                        <th class="text-center" scope="col">Departure station</th>
+                        <th class="text-center" scope="col">train code</th>
+                        <th class="text-center" scope="col">wagon number</th>
+                        <th class="text-center" scope="col">On time</th>
+                        <th class="text-center" scope="col">Cancelled</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -23,19 +23,19 @@
                             <td>{{ $train->agency }}</td>
                             <td>{{ $train->departure_station }}</td>
                             <td>{{ $train->arrival_station }}</td>
-                            <td>{{ $train->departure_date }}</td>
-                            <td>{{ $train->arrival_eta }}</td>
-                            <td>n° {{ $train->train_code }}</td>
-                            <td>{{ $train->n_wagons }}</td>
+                            <td class="text-center">{{ $train->departure_date }}</td>
+                            <td class="text-center">{{ $train->arrival_eta }}</td>
+                            <td class="text-center">n° {{ $train->train_code }}</td>
+                            <td class="text-center">{{ $train->n_wagons }}</td>
                             @if ($train->on_time == 1)
-                                <td class="text-uppercase text-success">yes</td>
+                                <td class="text-uppercase text-success text-center">yes</td>
                             @else
-                                <td class="text-uppercase text-danger">late</td>
+                                <td class="text-uppercase text-danger text-center">late</td>
                             @endif
                             @if ($train->cnx == 1)
-                                <td class="text-uppercase text-danger">yes</td>
+                                <td class="text-uppercase text-danger text-center">yes</td>
                             @else
-                                <td class="text-uppercase text-success">no</td>
+                                <td class="text-uppercase text-success text-center">no</td>
                             @endif
                         </tr>
                     @endforeach
